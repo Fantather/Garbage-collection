cc8bd903b2f178a9d1f65058901ca5a216e02f1c
Алексей Дириволков <dirivolkova@gmail.com>
2025-08-19 18:00:25 +0300

    Добавьте файлы проекта.

---
+++ Garbage collection/Task 1/Play.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Задание 1
+//Создайте класс Пьеса. Класс должен хранить такую информацию:
+
+//Название пьесы;
+//ФИО автора;
+//Жанр;
+//Год выпуска.
+//Реализуйте в классе методы и свойства, необходимые для функционирования класса. Добавьте в классе деструктор.
+//Напишите код для тестирования функциональности класса.
+//Напишите код для проверки работы деструктора (например, создание и удаление объектов).
+
+
+namespace Garbage_collection.Task_1
+{
+    internal class Play(string title, string authorFullName, string genre, DateTime releaseYear) : IDisposable, IDispose
+    {
+        public string Title { get; set; } = title;
+        public string AuthorFullName { get; set; } = authorFullName;
+        public string Genre { get; set; } = genre;
+        public DateTime ReleaseYear { get; set; } = releaseYear;
+
+        private bool disposed = false;
+
+        public void StartPlay() => Console.WriteLine($"Start play {Title}");
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected void Dispose(bool disposing)
+        {
+            if (disposed == true)
+            {
+                if (disposing == true)
+                {
+                    Console.WriteLine($"Пьеса подходит к концу");
+                }
+
+                Console.WriteLine($"The play {Title} is over, author {AuthorFullName} died");
+            }
+
+            disposed = true;
+        }
+
+        ~Play()
+        {
+            Console.WriteLine($"Уничтожение человечества произошло");
+            Dispose(false);
+        }
+    }
+}
---
+++ Garbage collection/Task 2/Store.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//Задание 2
+//Создайте класс Магазин
+//Класс должен хранить такую информацию:
+
+//Название магазина;
+//Адрес магазина;
+//Тип магазина:
+//Продовольственный;
+//Хозяйственный;
+//Одежда;
+//Обувь
+
+//Реализуйте в классе методы и свойства, необходимые для функционирования класса.
+//Класс должен реализовывать интерфейс IDisposable. Напишите код для тестирования функциональности класса.
+//Напишите код для вызова метода Dispose.
+
+
+namespace Garbage_collection.Task_2
+{
+    internal class Store (string name, string adress, string type) : IDisposable
+    {
+        public string Name { get; set; } = name;
+        public string Adress { get; set; } = adress;
+        public string Type { get; set; } = type;
+
+
+        private bool disposed = false;
+
+
+        public void Sales() => Console.WriteLine("Магазин открылся");
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected void Dispose(bool disposing)
+        {
+            if (disposed)
+            {
+                if (disposing) {
+                    Console.WriteLine("Произошло убийство");
+                }
+
+                Console.WriteLine("Магазин закрыт, до выяснения того что убийца - дворецкий");
+            }
+        }
+
+        ~Store()
+        {
+            Console.WriteLine("Дворецкого не нашли");
+            Dispose(false);
+        }
+    }
+}
